8a2d9fa9eebc9c685d986e4426c3fe8b7d134550
Gaurav Deshmukh <39861664+gauravsdeshmukh@users.noreply.github.com>
2018-07-27 23:51:15 +0530

    Add files via upload

---
+++ PlugFlowCrystallizer.py
@@ -0,0 +1,453 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  6 23:20:56 2018
+
+@author: Gaurav
+"""
+
+import scipy as sci
+import numba as nb
+import pandas as pd
+import os
+############################CLASS SPACE########################################
+class Boundary:
+    def __init__(self,boundary_type,boundary_value,constant=0):
+        self.DefineBoundary(boundary_type,boundary_value,constant)
+        
+    def DefineBoundary(self,boundary_type,boundary_value,constant):
+        self.type=boundary_type
+        self.value=boundary_value
+        self.constant=constant
+
+class Space:
+    def __init__(self):
+        pass
+    
+    def CreateMesh(self,rowpts,colpts):
+        self.rowpts=rowpts
+        self.colpts=colpts
+        self.u=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.v=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.p=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.T=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.C=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.SetEmptyMesh()
+        
+    def SetDeltas(self,breadth,length):
+        self.dx=length/self.colpts
+        self.dy=breadth/self.rowpts
+
+    def SetEmptyMesh(self):
+        self.u_next=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.v_next=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.T_next=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.C_next=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.p_c=sci.zeros((self.rowpts,self.colpts))
+        self.u_c=sci.zeros((self.rowpts,self.colpts))
+        self.v_c=sci.zeros((self.rowpts,self.colpts))
+        self.T_c=sci.zeros((self.rowpts,self.colpts))
+        self.C_c=sci.zeros((self.rowpts,self.colpts))
+
+class Fluid:
+    def __init__(self,rho,mu,k,cp,D):
+        self.SetFluidProperties(rho,mu)
+        self.SetThermalProperties(k,cp)
+        self.SetTransportProperties(D)
+    
+    def SetFluidProperties(self,rho,mu):
+        self.rho=rho
+        self.mu=mu
+        
+    def SetThermalProperties(self,k,cp):
+        self.k=k
+        self.cp=cp
+        self.alpha=self.k/(self.rho*self.cp)
+        
+    def SetTransportProperties(self,D):
+        self.D=D
+        
+        
+class Crystal:
+    def __init__(self,name,space):
+        self.name=name
+        self.space=space
+        self.rowpts=self.space.rowpts
+        self.colpts=self.space.colpts
+        self.GetSolubility()
+        self.SetMeshes()
+        
+    def GetSolubility(self):
+        cwdir=os.getcwd()
+        if(os.path.basename(os.getcwd())=="Result"):
+            os.chdir("..")
+        filename="SolubilityData.xlsx"
+        path=cwdir+"\\"+filename
+        sol=pd.read_excel(path)
+        
+        row=sol[sol["Name"]==self.name]
+        self.a0=row["a0"].values[0]
+        self.a1=row["a1"].values[0]
+        self.a2=row["a2"].values[0]
+        
+    def SetConstants(self,MW,kg,g,rho,dH):
+        self.MW=MW
+        self.kg=kg
+        self.g=g
+        self.rho=rho
+        self.dH=dH
+        
+    def SetMeshes(self):
+        self.N=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.L=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.G=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.Cstar=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.N_next=sci.zeros((self.rowpts+2,self.colpts+2))
+        self.L_next=sci.zeros((self.rowpts+2,self.colpts+2))
+    
+##########################BOUNDARY SPACE#######################################
+
+def SetUBoundary(space,left,right,top,bottom):
+    if(left.type=="D"):
+        space.u[:,0]=left.value
+    elif(left.type=="N"):
+        space.u[:,0]=-left.value*space.dx+space.u[:,1]
+    
+    if(right.type=="D"):
+        space.u[:,-1]=right.value
+    elif(right.type=="N"):
+        space.u[:,-1]=right.value*space.dx+space.u[:,-2]
+        
+    if(top.type=="D"):
+        space.u[-1,:]=2*top.value-space.u[-2,:]
+    elif(top.type=="N"):
+        space.u[-1,:]=top.value*space.dy+space.u[-2,:]
+     
+    if(bottom.type=="D"):
+        space.u[0,:]=2*bottom.value-space.u[1,:]
+    elif(bottom.type=="N"):
+        space.u[0,:]=-bottom.value*space.dy+space.u[1,:]
+        
+
+def SetVBoundary(space,left,right,top,bottom):
+    if(left.type=="D"):
+        space.v[:,0]=2*left.value-space.v[:,1]
+    elif(left.type=="N"):
+        space.v[:,0]=-left.value*space.dx+space.v[:,1]
+    
+    if(right.type=="D"):
+        space.v[:,-1]=2*right.value-space.v[:,-2]
+    elif(right.type=="N"):
+        space.v[:,-1]=right.value*space.dx+space.v[:,-2]
+        
+    if(top.type=="D"):
+        space.v[-1,:]=top.value
+    elif(top.type=="N"):
+        space.v[-1,:]=top.value*space.dy+space.v[-2,:]
+     
+    if(bottom.type=="D"):
+        space.v[0,:]=bottom.value
+    elif(bottom.type=="N"):
+        space.v[0,:]=-bottom.value*space.dy+space.v[1,:]
+    
+def SetPBoundary(space,left,right,top,bottom):
+    if(left.type=="D"):
+        space.p[:,0]=left.value
+    elif(left.type=="N"):
+        space.p[:,0]=-left.value*space.dx+space.p[:,1]
+    
+    if(right.type=="D"):
+        space.p[1,-1]=right.value
+    elif(right.type=="N"):
+        space.p[:,-1]=right.value*space.dx+space.p[:,-2]
+        
+    if(top.type=="D"):
+        space.p[-1,:]=top.value
+    elif(top.type=="N"):
+        space.p[-1,:]=top.value*space.dy+space.p[-2,:]
+     
+    if(bottom.type=="D"):
+        space.p[0,:]=bottom.value
+    elif(bottom.type=="N"):
+        space.p[0,:]=-bottom.value*space.dy+space.p[1,:]
+    
+ 
+def SetTBoundary(space,left,right,top,bottom):
+    if(left.type=="D"):
+        space.T[:,0]=left.value
+    elif(left.type=="N"):
+        space.T[:,0]=-left.value*space.dx+space.T[:,1]
+    elif(left.type=="C"):
+        space.T[:,0]=left.value*space.T[:,1]+left.constant
+    
+    if(right.type=="D"):
+        space.T[1,-1]=right.value
+    elif(right.type=="N"):
+        space.T[:,-1]=right.value*space.dx+space.T[:,-2]
+    elif(right.type=="C"):
+        space.T[:,-1]=right.value*space.T[:,-2]-right.constant
+        
+    if(top.type=="D"):
+        space.T[-1,:]=top.value
+    elif(top.type=="N"):
+        space.T[-1,:]=top.value*space.dy+space.T[-2,:]
+    elif(top.type=="C"):
+        space.T[-1,:]=top.value*space.T[-2,:]-top.constant
+     
+    if(bottom.type=="D"):
+        space.T[0,:]=bottom.value
+    elif(bottom.type=="N"):
+        space.T[0,:]=-bottom.value*space.dy+space.T[1,:]
+    elif(bottom.type=="C"):
+        space.T[0,:]=bottom.value*space.T[1,:]+bottom.constant
+        
+def SetCBoundary(space,left,right,top,bottom):
+    if(left.type=="D"):
+        space.C[:,0]=left.value
+    elif(left.type=="N"):
+        space.C[:,0]=-left.value*space.dx+space.C[:,1]
+    elif(left.type=="C"):
+        space.C[:,0]=left.value*space.C[:,1]+left.constant
+    
+    if(right.type=="D"):
+        space.C[1,-1]=right.value
+    elif(right.type=="N"):
+        space.C[:,-1]=right.value*space.dx+space.C[:,-2]
+    elif(right.type=="C"):
+        space.C[:,-1]=right.value*space.C[:,-2]-right.constant
+        
+    if(top.type=="D"):
+        space.C[-1,:]=top.value
+    elif(top.type=="N"):
+        space.C[-1,:]=top.value*space.dy+space.C[-2,:]
+    elif(top.type=="C"):
+        space.C[-1,:]=top.value*space.C[-2,:]-top.constant
+     
+    if(bottom.type=="D"):
+        space.C[0,:]=bottom.value
+    elif(bottom.type=="N"):
+        space.C[0,:]=-bottom.value*space.dy+space.C[1,:]
+    elif(bottom.type=="C"):
+        space.C[0,:]=bottom.value*space.C[1,:]+bottom.constant 
+        
+########################FUNCTION SPACE#########################################
+def SetTimeStep(CFL,space,fluid):
+    if(space.u.any()!=0):
+        dt_hyper=CFL/max(sci.amax(space.u)/space.dx,sci.amax(space.v)/space.dy)
+    else:
+        dt_hyper=CFL*space.dx 
+        
+    dt_para=min(space.dx**2/(2*fluid.mu),space.dy**2/(2*fluid.mu))
+    dt_temp=min(space.dx**2/(2*fluid.alpha),space.dy**2/(2*fluid.alpha))
+    dt_conc=min(space.dx**2/(2*fluid.D),space.dy**2/(2*fluid.D))
+    dt_min=min(dt_hyper,dt_para,dt_temp,dt_conc)
+    space.dt=dt_min
+ 
+@nb.jit    
+def SolvePressurePoisson(space,fluid,left,right,top,bottom):
+    rows=int(space.rowpts)
+    cols=int(space.colpts)
+    u=space.u.astype(float)
+    v=space.v.astype(float)
+    p=space.p.astype(float,copy=False)
+    dx=float(space.dx)
+    dy=float(space.dy)
+    dt=float(space.dt)
+    rho=float(fluid.rho)
+    factor=1/(2/dx**2+2/dy**2)
+
+    error=1
+    tol=1e-3
+
+    i=0
+    while(error>tol):
+        i+=1
+        p_old=p.astype(float,copy=True)
+        
+        term_1=(1/dt)*(((v[2:,1:cols+1]-v[0:rows,1:cols+1])/(2*dy))+((u[1:rows+1,2:]-u[1:rows+1,0:cols])/(2*dx)))
+        term_2=((u[1:rows+1,2:]-u[1:rows+1,0:cols])/(2*dx))**2
+        term_3=((v[2:,1:cols+1]-v[0:rows,1:cols+1])/(2*dy))**2
+        term_4=2*((u[2:,1:cols+1]-u[0:rows,1:cols+1])/(2*dy))*((v[1:rows+1,2:]-v[1:rows+1,0:cols])/(2*dx))
+        term_5=(p_old[2:,1:cols+1]+p_old[0:rows,1:cols+1])/dy**2+(p_old[1:rows+1,2:]+p_old[1:rows+1,0:cols])/dx**2
+        p[1:rows+1,1:cols+1]=factor*term_5-(factor*rho)*(term_1-term_2-term_3-term_4)
+        error=sci.amax(abs(p-p_old))
+        #Apply Boundary Conditions
+        SetPBoundary(space,left,right,top,bottom)
+        if(i>500):
+            tol*=10
+            
+    
+@nb.jit
+def SolveMomentumEquation(space,fluid):
+    rows=int(space.rowpts)
+    cols=int(space.colpts)
+    u=space.u.astype(float,copy=False)
+    v=space.v.astype(float,copy=False)
+    p=space.p.astype(float)
+    dx=float(space.dx)
+    dy=float(space.dy)
+    dt=float(space.dt)
+    rho=float(fluid.rho)
+    mu=float(fluid.mu)
+    u_next=space.u_next.astype(float,copy=False)
+    v_next=space.v_next.astype(float,copy=False)
+
+    u1_y=(u[2:,1:cols+1]-u[0:rows,1:cols+1])/(2*dy)
+    u1_x=(u[1:rows+1,2:]-u[1:rows+1,0:cols])/(2*dx)
+    p1_x=(p[1:rows+1,2:]-p[1:rows+1,0:cols])/(2*dx)
+    u2_y=(u[2:,1:cols+1]-2*u[1:rows+1,1:cols+1]+u[0:rows,1:cols+1])/(dy**2)
+    u2_x=(u[1:rows+1,2:]-2*u[1:rows+1,1:cols+1]+u[1:rows+1,0:cols])/(dx**2)
+    v_face=(v[1:rows+1,1:cols+1]+v[1:rows+1,0:cols]+v[2:,1:cols+1]+v[2:,0:cols])/4
+    u_next[1:rows+1,1:cols+1]=u[1:rows+1,1:cols+1]-dt*(u[1:rows+1,1:cols+1]*u1_x+v_face*u1_y)-(dt/rho)*p1_x+(dt*(mu/rho)*(u2_x+u2_y))   
+
+    v1_y=(v[2:,1:cols+1]-v[0:rows,1:cols+1])/(2*dy)
+    v1_x=(v[1:rows+1,2:]-v[1:rows+1,0:cols])/(2*dx)
+    p1_y=(p[2:,1:cols+1]-p[0:rows,1:cols+1])/(2*dy)
+    v2_y=(v[2:,1:cols+1]-2*v[1:rows+1,1:cols+1]+v[0:rows,1:cols+1])/(dy**2)
+    v2_x=(v[1:rows+1,2:]-2*v[1:rows+1,1:cols+1]+v[1:rows+1,0:cols])/(dx**2)
+    u_face=(u[1:rows+1,1:cols+1]+u[1:rows+1,2:]+u[0:rows,1:cols+1]+u[0:rows,2:])/4
+    v_next[1:rows+1,1:cols+1]=v[1:rows+1,1:cols+1]-dt*(u_face*v1_x+v[1:rows+1,1:cols+1]*v1_y)-(dt/rho)*p1_y+(dt*(mu/rho)*(v2_x+v2_y))
+            
+def AdjustUV(space):
+    space.u[1:-1,1:-1]=space.u_next[1:-1,1:-1].copy()
+    space.v[1:-1,1:-1]=space.v_next[1:-1,1:-1].copy()
+    
+def SetCentrePUVTCNL(space,crystal):
+    space.p_c=space.p[1:-1,1:-1]
+    space.u_c=space.u[1:-1,1:-1]
+    space.v_c=space.v[1:-1,1:-1]
+    space.T_c=space.T[1:-1,1:-1]
+    space.C_c=space.C[1:-1,1:-1]
+    crystal.N_c=crystal.N[1:-1,1:-1]
+    crystal.L_c=crystal.L[1:-1,1:-1]
+   
+def SetInitialT(space,T_int):
+    space.T[:,:]=T_int    
+        
+@nb.jit
+def SolveEnergyEquation(space,crystal,fluid):    
+    rows=int(space.rowpts)
+    cols=int(space.colpts)
+    u=space.u.astype(float,copy=False)
+    v=space.v.astype(float,copy=False)
+    T=space.T.astype(float,copy=False)
+    T_next=space.T_next.astype(float,copy=False)
+    dx=float(space.dx)
+    dy=float(space.dy)
+    dt=float(space.dt)
+    alpha=float(fluid.alpha)
+    cp=float(fluid.cp)
+    rho=float(fluid.rho)
+    rhoc=float(crystal.rho)
+    dH=float(crystal.dH)
+    G=crystal.G.astype(float)
+    N=crystal.N.astype(float)
+    
+    T1_y=(T[2:,1:cols+1]-T[0:rows,1:cols+1])/(2*dy)
+    T1_x=(T[1:rows+1,2:]-T[1:rows+1,0:cols])/(2*dx)
+    T2_y=(T[2:,1:cols+1]-2*T[1:rows+1,1:cols+1]+T[0:rows,1:cols+1])/(dy**2)
+    T2_x=(T[1:rows+1,2:]-2*T[1:rows+1,1:cols+1]+T[1:rows+1,0:cols])/(dx**2)
+    source=-((rhoc*dH)/(rho*cp))*G[1:rows+1,1:cols+1]*N[1:rows+1,1:cols+1]
+    T_next[1:rows+1,1:cols+1]=T[1:rows+1,1:cols+1]-dt*(u[1:rows+1,1:cols+1]*T1_x+v[1:rows+1,1:cols+1]*T1_y)+(dt*alpha*(T2_x+T2_y))+(dt*source)
+    
+def AdjustT(space):
+    space.T[1:-1,1:-1]=space.T_next[1:-1,1:-1].copy()
+    
+def SetInitialC(space,C_int):
+    space.C[:,:]=C_int    
+ 
+@nb.jit
+def SolveSpeciesEquation(space,crystal,fluid):
+    rows=int(space.rowpts)
+    cols=int(space.colpts)
+    u=space.u.astype(float,copy=False)
+    v=space.v.astype(float,copy=False)
+    C=space.C.astype(float,copy=False)
+    C_next=space.C_next.astype(float,copy=False)
+    dx=float(space.dx)
+    dy=float(space.dy)
+    dt=float(space.dt)
+    D=float(fluid.D)
+    rhoc=float(crystal.rho)
+    G=crystal.G.astype(float)
+    N=crystal.N.astype(float)
+    
+    C1_y=(C[2:,1:cols+1]-C[0:rows,1:cols+1])/(2*dy)
+    C1_x=(C[1:rows+1,2:]-C[1:rows+1,0:cols])/(2*dx)
+    C2_y=(C[2:,1:cols+1]-2*C[1:rows+1,1:cols+1]+C[0:rows,1:cols+1])/(dy**2)
+    C2_x=(C[1:rows+1,2:]-2*C[1:rows+1,1:cols+1]+C[1:rows+1,0:cols])/(dx**2)
+    source=-rhoc*G[1:rows+1,1:cols+1]*N[1:rows+1,1:cols+1]
+    C_next[1:rows+1,1:cols+1]=C[1:rows+1,1:cols+1]-dt*(u[1:rows+1,1:cols+1]*C1_x+v[1:rows+1,1:cols+1]*C1_y)+(dt*D*(C2_x+C2_y))+(dt*source)
+    
+def AdjustC(space):
+    space.C[1:-1,1:-1]=space.C_next[1:-1,1:-1].copy()  
+    
+def GetCstar(space,crystal):
+    T=space.T.astype(float,copy=True)
+    T-=273.16
+    Cstar_unmodded=crystal.a0+crystal.a1*T+crystal.a2*T**2
+    Cstar=(Cstar_unmodded/crystal.MW)/10/1000 #mol/m3
+    crystal.Cstar=Cstar.copy()
+
+def GetG(space,crystal):
+    kg=float(crystal.kg)
+    g=float(crystal.g)
+    C=space.C.astype(float)
+    Cstar=crystal.Cstar.astype(float)
+    G=kg*(C-Cstar)**g
+    crystal.G=G.copy()
+
+def SetInitialNL(crystal,N_init,L_init):
+    crystal.N[:,0]=N_init
+    crystal.L[:,0]=L_init
+    crystal.N_next[:,0]=N_init
+    crystal.L_next[:,0]=L_init
+
+@nb.jit
+def SolvePopulationBalanceEquation(space,crystal):
+    rows=int(space.rowpts)
+    cols=int(space.colpts)
+    u=space.u.astype(float,copy=False)
+    dx=float(space.dx)
+    dt=float(space.dt)
+    G=crystal.G.astype(float)
+    N=crystal.N.astype(float)
+    L=crystal.L.astype(float)
+    N_next=crystal.N_next.astype(float,copy=False)
+    L_next=crystal.L_next.astype(float,copy=False)
+    
+    N1_x=(N[1:rows+1,1:cols+1]-N[1:rows+1,0:cols])/dx
+    N_next[1:rows+1,1:cols+1]=N[1:rows+1,1:cols+1]-(dt*u[1:rows+1,1:cols+1]*N1_x)
+    
+    L1_x=(L[1:rows+1,1:cols+1]-L[1:rows+1,0:cols])/dx
+    L_next[1:rows+1,1:cols+1]=L[1:rows+1,1:cols+1]-(dt*u[1:rows+1,1:cols+1]*L1_x)+(dt*G[1:rows+1,1:cols+1]*N[1:rows+1,1:cols+1])
+
+def AdjustNL(crystal):
+    crystal.N=crystal.N_next.copy()
+    crystal.L=crystal.L_next.copy()
+    
+    
+def WriteToFile(space,crystal,iteration,interval):
+    if(iteration%interval==0):
+        cwdir=os.getcwd()
+        if(iteration==0):
+            if(os.path.isdir("Result")==False):
+                os.mkdir("Result")
+                os.chdir("Result")
+                cwdir=os.getcwd()
+            elif(os.path.isdir("Result")==True):
+                os.chdir("Result")
+                cwdir=os.getcwd()
+                filelist=os.listdir()
+                for file in filelist:
+                    os.remove(file)
+        if(os.path.basename(os.getcwd())!="Result"):
+            os.chdir("Result")
+            cwdir=os.getcwd()
+        filename=f"PUV{iteration}.txt"
+        path=os.path.join(cwdir,filename)
+        with open(path,"w") as f:
+            for i in range(space.rowpts):
+                for j in range(space.colpts):
+                    f.write("{}\t{}\t{}\t{}\t{}\t{}\t".format(space.P_c[i,j],space.u_c[i,j],space.v_c[i,j],space.T_c[i,j],space.C_c[i,j],crystal.L_c[i,j]))
+    
+#######################END OF FILE#############################################        
---
+++ PlugFlowCrystallizer_Input.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 17:33:26 2018
+
+@author: Gaurav
+"""
+
+import scipy as sci
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from PlugFlowCrystalizer import *
+
+###############################################################################
+###########################USER INPUT BEGINS###################################
+############################DEFINE SPATIAL AND TEMPORAL PARAMETERS#############
+length=4
+breadth=0.4
+colpts=120
+rowpts=25
+time=100
+crystal_timedelay=4 #The time after which crystals are introduced in the system
+###############################MISC############################################
+CFL_number=0.1 #Do not touch this unless solution diverges
+file_flag=0 #Keep 1 to print results to file
+plot_flag=1 #Keep 1 to plot results at the end
+file_interval=10 #Regular time interval after which files are written
+###########################DEFINE PHYSICAL PARAMETERS##########################
+rho=1
+mu=0.01
+k=0.5
+cp=100
+D=1e-2
+###########################DEFINE CRYSTAL PARAMETERS###########################
+MW=101.1032
+kg=1.1612*10**(-4)
+g=1.32
+rhoc=2.1
+dH=-1000
+##########################DEFINE INITIAL MOMENTUM PARAMETERS###################
+u_in=1
+v_wall=0
+p_out=0
+##########################DEFINE INITIAL TEMPERATURE PARAMETERS###############
+h=3000
+T_jacket=303
+T_in=363
+T_init=293
+##########################DEFINE INITIAL CONCENTRATION CONDITIONS##############
+C_init=0
+C_in=0.1
+#########################DEFINE INITIAL CRYSTAL CONDITIONS#####################
+N_init=1000
+L_cryst=1e-5
+L_init=N_init*L_cryst
+
+###############################################################################
+########################CREATE SPACE OBJECT####################################
+pfc=Space()
+pfc.CreateMesh(rowpts,colpts)
+pfc.SetDeltas(breadth,length)
+water=Fluid(rho,mu,k,cp,D)
+
+###############################################################################
+#########################BOUNDARY DEFINITIONS##################################
+########################HEAT CONVECTION BOUNDARY###############################
+jacketvaluetop=1/(1-(h*pfc.dy/water.k))
+jacketconstanttop=(h*pfc.dy/water.k)/(1-(h*pfc.dy/water.k))*T_jacket
+jacketvaluebottom=1/(1+(h*pfc.dy/water.k))
+jacketconstantbottom=(h*pfc.dy/water.k)/(1+(h*pfc.dy/water.k))*T_jacket
+########################CREATE BOUNDARY OBJECTS################################
+###########################VELOCITY############################################
+flow=Boundary("D",u_in)
+noslip=Boundary("D",v_wall)
+zeroflux=Boundary("N",0)
+############################PRESSURE###########################################
+pressureatm=Boundary("D",p_out)
+############################TEMPERATURE########################################
+jackettop=Boundary("C",jacketvaluetop,jacketconstanttop)
+jacketbottom=Boundary("C",jacketvaluebottom,jacketconstantbottom)
+intemp=Boundary("D",T_in)
+############################CONCENTRATION######################################
+inconc=Boundary("D",C_in)
+
+###############################################################################
+#########################CREATE CRYSTAL OBJECT#################################
+kno3=Crystal("Potassium nitrate",pfc)
+kno3.SetConstants(MW,kg,g,rhoc,dH)
+
+###############################################################################
+#########################SET INITIAL CONDITIONS IN DOMAIN######################
+SetInitialT(pfc,T_init)
+SetInitialC(pfc,C_init)
+SetInitialNL(kno3,N_init,L_init)
+
+#######################USER INPUT ENDS#########################################
+###############################################################################
+#############################INITIALIZATION####################################
+t=0
+i=0
+############################THE RUN############################################
+while(t<time):
+    CFL=CFL_number
+    SetTimeStep(CFL,pfc,water)
+    timestep=pfc.dt
+    
+    SetUBoundary(pfc,flow,zeroflux,noslip,noslip)
+    SetVBoundary(pfc,zeroflux,zeroflux,noslip,noslip)
+    SetPBoundary(pfc,zeroflux,pressureatm,zeroflux,zeroflux)
+    SetTBoundary(pfc,intemp,zeroflux,jackettop,jacketbottom)
+    SetCBoundary(pfc,inconc,zeroflux,zeroflux,zeroflux)
+    
+    SolvePressurePoisson(pfc,water,zeroflux,pressureatm,zeroflux,zeroflux)
+    SolveMomentumEquation(pfc,water)
+    AdjustUV(pfc)
+    
+    SolveEnergyEquation(pfc,kno3,water)
+    AdjustT(pfc)
+    
+    SolveSpeciesEquation(pfc,kno3,water)
+    AdjustC(pfc)
+    
+    if(t>crystal_timedelay):
+        GetCstar(pfc,kno3)
+        GetG(pfc,kno3)
+        
+        SolvePopulationBalanceEquation(pfc,kno3)
+        AdjustNL(kno3)
+    
+    SetCentrePUVTCNL(pfc,kno3)
+    if(file_flag==1):
+        WriteToFile(pfc,kno3,i,file_interval)
+    
+    u=pfc.u
+    v=pfc.v
+    p=pfc.p
+    T=pfc.T
+    C=pfc.C
+    G=kno3.G
+    N=kno3.N
+    L=kno3.L
+    u_c=pfc.u_c
+    v_c=pfc.v_c
+    p_c=pfc.p_c
+    T_c=pfc.T_c
+    C_c=pfc.C_c
+    N_c=kno3.N_c
+    L_c=kno3.L_c
+    t+=timestep
+    i+=1
+    print(time-t)
+
+###########################END OF RUN##########################################
+###############################################################################
+#######################SET ARRAYS FOR PLOTTING#################################
+x=sci.linspace(0,length,colpts)
+y=sci.linspace(0,breadth,rowpts)
+[X,Y]=sci.meshgrid(x,y)
+
+u=pfc.u
+v=pfc.v
+p=pfc.p
+T=pfc.T
+C=pfc.C
+G=kno3.G
+N=kno3.N
+L=kno3.L
+u_c=pfc.u_c
+v_c=pfc.v_c
+p_c=pfc.p_c
+T_c=pfc.T_c
+C_c=pfc.C_c
+N_c=kno3.N_c
+L_c=kno3.L_c
+
+######################EXTRA PLOTTING CODE BELOW################################
+if(plot_flag==1):
+    plt.figure(figsize=(16,8))
+    plt.contourf(X,Y,p_c,cmap=cm.viridis)
+    plt.colorbar()
+    plt.quiver(X,Y,u_c,v_c)
+    plt.title("Velocity and Pressure Plot")
+    
+    plt.figure(figsize=(16,8))
+    plt.contourf(X,Y,T_c,cmap=cm.inferno)
+    plt.colorbar()
+    plt.title("Temperature Plot")
+    
+    plt.figure(figsize=(16,8))
+    plt.contourf(X,Y,C_c,cmap=cm.viridis)
+    plt.colorbar()
+    plt.title("Concentration Plot")
+    
+    plt.figure(figsize=(16,8))
+    plt.contourf(X,Y,L_c,cmap=cm.viridis)
+    plt.colorbar()
+    plt.title("Crystal Length Plot")
+
+
+
+
+###########################END OF FILE#########################################
